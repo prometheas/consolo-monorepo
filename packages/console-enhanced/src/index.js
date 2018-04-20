036b9067bef96b2dd360661e85458030ebe71e64
John Lianoglou <>
2018-04-20 14:35:03 -0400

    Improving readibily of log() method

--- packages/console-enhanced/src/index.js
+++ packages/console-enhanced/src/index.js
@@ -18,8 +18,18 @@ export const enhanceConsole = () => {
   console.consoloEnhanced = true;
 };
 
+export const extractLogLevelFromArgs = (args, levels) => (
+  (
+    args.length > 1
+    && levels.includes(args[0])
+  ) ? args.shift() : undefined
+);
+
 export const log = (...args) => {
-  let level = (args.length > 1) ? args.shift() : undefined;
+  let level = extractLogLevelFromArgs(
+    args,
+    ['debug', 'error', 'info', 'warn'],
+  );
 
   if (!level) {
     level = 'info';
@@ -31,12 +41,6 @@ export const log = (...args) => {
 export const restoreConsole = () => {
   delete console.consoloEnhanced;
 
-  console.debug = preservedConsoleMethods.debug;
-  console.error = preservedConsoleMethods.error;
-  console.info = preservedConsoleMethods.info;
-  console.log = preservedConsoleMethods.log;
-  console.warn = preservedConsoleMethods.warn;
-};
   console.debug = preservedConsoleMethods.debug;
   console.error = preservedConsoleMethods.error;
   console.info = preservedConsoleMethods.info;
