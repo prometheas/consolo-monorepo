9a4c4fba47c158b41c6e4ff308dd793a24549b89
John Lianoglou <>
2018-05-12 15:35:18 -0400

    Added BaseAdaptor class

---
+++ packages/consolo/src/BaseAdaptor.js
@@ -0,0 +1,17 @@
+export default class BaseAdaptor {
+  enhanceConsole(target) {
+    /* eslint-disable no-param-reassign */
+    target.log = this.log;
+    target.debug = (...args) => (this.log('debug', ...args));
+    target.error = (...args) => (this.log('error', ...args));
+    target.info = (...args) => (this.log('info', ...args));
+    target.warn = (...args) => (this.log('warn', ...args));
+    /* eslint-enable no-param-reassign */
+  }
+
+  /* eslint-disable class-methods-use-this */
+  log() {
+    throw Error('Consolo\'s BaseAdaptor must be subclassed and its #log() method overridden');
+  }
+  /* eslint-enable class-methods-use-this */
+}
