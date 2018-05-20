157eacde33cfdb8840a31d678fbbce0aa96b08bf
John Lianoglou <>
2018-05-19 20:12:08 -0400

    Experimenting with dynamic require

--- packages/consolo/index.js
+++ packages/consolo/index.js
@@ -1,1 +1,5 @@
-module.exports = require('./dist/index');
+const path = (process.env.CONSOLO_TESTING) ? './src/index' : './dist/index';
+
+/* eslint-disable import/no-dynamic-require */
+module.exports = require(path);
+/* eslint-enable import/no-dynamic-require */
