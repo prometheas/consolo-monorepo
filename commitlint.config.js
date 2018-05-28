2fbcb79763f972b71a1b2b6d52607e83180e015f
John Lianoglou <>
2018-05-27 20:31:51 -0400

    chore: adding custom commit type "wip"

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,24 @@
-export default {
+const typeValues = [
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test',
+  'wip',
+];
+
+module.exports = {
   extends: [
     '@commitlint/config-conventional',
     '@commitlint/config-lerna-scopes',
   ],
+  rules: {
+    'type-enum': [2, 'always', typeValues],
+  },
 };
